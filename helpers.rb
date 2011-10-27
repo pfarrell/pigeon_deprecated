615fadc85ec6f9b798ef50219c38b3b9984ff8da
Patrick Farrell <mr.patfarrell@gmail.com>
2011-10-27 14:46:04 -0400

    prototype working

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,4 @@
-require 'mail'
+%w(mail uri).each { |dependency| require dependency }
 
 def String.random_alphanumeric(size=16)
   s = ""
@@ -46,10 +46,17 @@ end
 def find_links!(part, links)
   arr = part.scan(/http[s]?:\/\/[a-zA-Z0-9\/\.-]*/)
   arr.each do |entry|
-    links[entry] = entry
+    if /www\.w3\.org/.match(entry).nil? \
+      and /schemas\.microsoft/.match(entry).nil?
+      links[entry] = entry
+    end
   end
 end
 
 def save_page(link) 
-  system("wget -r -q -np --random-wait -p -l 1 -k -P " + String.random_alphanumeric() + " " + link)
+  mkdir!("sites")
+  dir = String.random_alphanumeric()
+  uri = URI.parse(link)
+  system("wget -r -p -q --progress=dot:binary -np --domains " + uri.host + " --random-wait -p -l 1 -k -P sites/" + dir + " " + link)
+  File.open("sites/index.html", 'a') {|f| f.write("<div><a href='" + dir + '/' + uri.host + '/' + uri.path + "'>" + link + "</a></div>")}
 end
