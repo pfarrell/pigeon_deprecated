23657c80877b3692c4f466783b7a5d6fc4528585
Patrick Farrell <pfarrell@ubuntu.(none)>
2012-01-12 07:54:24 -0800

    added tests for wget_filename

--- helpers_test.rb
+++ helpers_test.rb
@@ -113,115 +113,104 @@ class HelpersTest < Test::Unit::TestCase
     assert_equal(random_dir('.').length, 18)
   end
 
-  def test_wget_filename_1
-    assert_equal(wget_filename('http://news.ycombinator.com/item?id=3349390'), 'item%3Fid%3D3349390.html')
-  end
+	def test_wget_filename_1
+		assert_equal(wget_filename('http://news.ycombinator.com/item?id=3349390'), 'item%3Fid%3D3349390.html')
+	end
 
-  def test_wget_filename_2
-    assert_equal(wget_filename('http://blog.firsthand.ca/2011/10/rails-is-not-your-application.html'), '')
-  end
+	def test_wget_filename_2
+		assert_equal(wget_filename('http://blog.firsthand.ca/2011/10/rails-is-not-your-application.html'), 'rails-is-not-your-application.html')
+	end
 
-  def test_wget_filename_3
-    assert_equal(wget_filename('http://www.infoq.com/presentations/We-Really-Dont-Know-How-To-Compute'), '')
-  end
+	def test_wget_filename_3
+		assert_equal(wget_filename('http://www.infoq.com/presentations/We-Really-Dont-Know-How-To-Compute'), 'We-Really-Dont-Know-How-To-Compute.html')
+	end
 
-  def test_wget_filename_4
-		assert_equal(wget_filename('http://blog.fogus.me/2011/08/14/perlis-languages/'), '')
-  end
+	def test_wget_filename_4
+		assert_equal(wget_filename('http://blog.fogus.me/2011/08/14/perlis-languages/'), 'index.html')
+	end
 
-  def test_wget_filename_5
-		assert_equal(wget_filename('http://www.mandolincafe.com/cgi-bin/tab/searchdb.cgi?searchterm=jjgs'), '')
-  end
+	def test_wget_filename_5
+		assert_equal(wget_filename('http://www.mandolincafe.com/cgi-bin/tab/searchdb.cgi?searchterm=jjgs'), 'searchdb.cgi%3Fsearchterm%3Djjgs.html')
+	end
 
-  def test_wget_filename_6
-		assert_equal(wget_filename('http://www.alincoln-library.com/abraham-lincoln-jokes.shtml'), '')
-  end
+	def test_wget_filename_6
+		assert_equal(wget_filename('http://www.alincoln-library.com/abraham-lincoln-jokes.shtml'), 'abraham-lincoln-jokes.shtml.html')
+	end
 
-  def test_wget_filename_7
-		assert_equal(wget_filename('http://msdn.microsoft.com/en-us/magazine/cc164014.aspx'), '')
-  end
+	def test_wget_filename_7
+		assert_equal(wget_filename('http://msdn.microsoft.com/en-us/magazine/cc164014.aspx'), 'cc164014.aspx.html')
+	end
 
-  def test_wget_filename_8
-		assert_equal(wget_filename('http://arstechnica.com/gadgets/news/2011/10/facebook-sees-600000-compromised-logins-per-day006-of-all-logins.ars'), '')
-  end
+	def test_wget_filename_8
+		assert_equal(wget_filename('http://arstechnica.com/gadgets/news/2011/10/facebook-sees-600000-compromised-logins-per-day006-of-all-logins.ars'), 'facebook-sees-600000-compromised-logins-per-day006-of-all-logins.ars.html')
+	end
 
-  def test_wget_filename_9
-		assert_equal(wget_filename('http://alternet.us.com/?p=1398'), '')
-  end
+	def test_wget_filename_9
+		assert_equal(wget_filename('http://www.gamasutra.com/blogs/ChrisHildenbrand/20111027/8713/2D_Game_Art_For_Programmers__Part_3.php'), '2D_Game_Art_For_Programmers__Part_3.php.html')
+	end
 
-  def test_wget_filename_10
-		assert_equal(wget_filename('http://www.gamasutra.com/blogs/ChrisHildenbrand/20111027/8713/2D_Game_Art_For_Programmers__Part_3.php'), '')
-  end
+	def test_wget_filename_10
+		assert_equal(wget_filename('http://www.cl.cam.ac.uk/teaching/1112/L100/introling.pdf'), 'introling.pdf')
+	end
 
-  def test_wget_filename_11
-		assert_equal(wget_filename('http://www.cl.cam.ac.uk/teaching/1112/L100/introling.pdf'), '')
-  end
+	def test_wget_filename_11
+		assert_equal(wget_filename('http://martinfowler.com/articles/lmax.html?t=1319912579'), 'lmax.html%3Ft%3D1319912579')
+	end
 
-  def test_wget_filename_12
-		assert_equal(wget_filename('http://martinfowler.com/articles/lmax.html?t=1319912579'), '')
-  end
+	def test_wget_filename_12
+		assert_equal(wget_filename('http://robonobo.com/'), 'index.html')
+	end
 
-  def test_wget_filename_13
-		assert_equal(wget_filename('http://robonobo.com/'), '')
-  end
+	def test_wget_filename_13
+		assert_equal(wget_filename('http://blog.mailgun.net/post/12482374892/handle-incoming-emails-like-a-pro-mailgun-api-2-0'), 'handle-incoming-emails-like-a-pro-mailgun-api-2-0.html')
+	end
 
-  def test_wget_filename_14
-		assert_equal(wget_filename('http://blog.mailgun.net/post/12482374892/handle-incoming-emails-like-a-pro-mailgun-api-2-0'), '')
-  end
+	def test_wget_filename_14
+		assert_equal(wget_filename('http://www.newyorker.com/reporting/2011/11/14/111114fa_fact_gladwell?currentPage=all'), '111114fa_fact_gladwell%3FcurrentPage%3Dall.html')
+	end
 
-  def test_wget_filename_15
-		assert_equal(wget_filename('http://www.newyorker.com/reporting/2011/11/14/111114fa_fact_gladwell?currentPage=all'), '')
-  end
+	def test_wget_filename_15
+		assert_equal(wget_filename('http://www.jfsowa.com/logic/math.htm'), 'math.htm.html')
+	end
 
-  def test_wget_filename_16
-		assert_equal(wget_filename('http://www.jfsowa.com/logic/math.htm'), '')
-  end
+	def test_wget_filename_16
+		assert_equal(wget_filename('http://queue.acm.org/detail.cfm?id=2068896'), 'detail.cfm%3Fid%3D2068896.html')
+	end
 
-  def test_wget_filename_17
-		assert_equal(wget_filename('http://queue.acm.org/detail.cfm?id=2068896'), '')
-  end
+	def test_wget_filename_17
+		assert_equal(wget_filename('http://en.wikipedia.org/wiki/Parrondo%27s_paradox'), 'Parrondo%2527s_paradox.html')
+	end
 
-  def test_wget_filename_18
-		assert_equal(wget_filename('http://en.wikipedia.org/wiki/Parrondo%27s_paradox'), '')
-  end
+	def test_wget_filename_18
+		assert_equal(wget_filename('http://www.nytimes.com/2011/11/20/business/after-law-school-associates-learn-to-be-lawyers.html?_r=2&ref=business&pagewanted=all'), 'after-law-school-associates-learn-to-be-lawyers.html%3F_r%3D2%26ref%3Dbusiness%26pagewanted%3Dall')
+	end
 
-  def test_wget_filename_19
-		assert_equal(wget_filename('http://www.nytimes.com/2011/11/20/business/after-law-school-associates-learn-to-be-lawyers.html?_r=2&ref=business&pagewanted=all'), '')
-  end
+	def test_wget_filename_19
+		assert_equal(wget_filename('http://www.catonmat.net/download/perl1line.txt'), 'perl1line.txt.html')
+	end
 
-  def test_wget_filename_20
-		assert_equal(wget_filename('http://www.catonmat.net/download/perl1line.txt'), '')
-  end
+	def test_wget_filename_20
+		assert_equal(wget_filename('https://github.com/TTimo/doom3.gpl'), 'doom3.gpl.html')
+	end
 
-  def test_wget_filename_21
-		assert_equal(wget_filename('http://www.ultimate-guitar.com/print.php/id1001463/?transpose=3D0'), '')
-  end
+	def test_wget_filename_22
+		assert_equal(wget_filename('http://introducinghtml5.com/'), 'index.html')
+	end
 
-  def test_wget_filename_22
-		assert_equal(wget_filename('https://github.com/TTimo/doom3.gpl'), '')
-  end
+	def test_wget_filename_23
+		assert_equal(wget_filename('http://m.techcrunch.com/2011/11/23/marketing-software-giant-exacttarget-re-files-for-ipo-will-raise-100m/?icid=tc_home_art&'), '%3Ficid%3Dtc_home_art%26.html')
+	end
 
-  def test_wget_filename_23
-		assert_equal(wget_filename('http://introducinghtml5.com/'), '')
-  end
+	def test_wget_filename_24
+		assert_equal(wget_filename('http://c2.com/cgi/wiki?SwitchStatementsSmell'), 'wiki%3FSwitchStatementsSmell.html')
+	end
 
-  def test_wget_filename_24
-		assert_equal(wget_filename('http://m.techcrunch.com/2011/11/23/marketing-software-giant-exacttarget-re-files-for-ipo-will-raise-100m/?icid=tc_home_art&'), '')
-  end
+	def test_wget_filename_25
+		assert_equal(wget_filename('http://twinpeaks.org/faqeps.htm#e5'), 'faqeps.htm.html')
+	end
 
-  def test_wget_filename_25
-		assert_equal(wget_filename('http://c2.com/cgi/wiki?SwitchStatementsSmell'), '')
-  end
-
-  def test_wget_filename_26
-		assert_equal(wget_filename('http://twinpeaks.org/faqeps.htm#e5'), '')
-  end
-
-  def test_wget_filename_27
-		assert_equal(wget_filename('http://regex.learncodethehardway.org/?'), '')
-  end
-
-  def test_wget_filename_28
-		assert_equal(wget_filename('http://news.ycombinator.com/item?id=3D3349390</a></span>'), '')
-  end
+	def test_wget_filename_26
+		assert_equal(wget_filename('http://regex.learncodethehardway.org/?'), '%3F.html')
+	end
 end
+
