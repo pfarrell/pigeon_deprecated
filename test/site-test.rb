e04f8cbdfbbceafbafa2f19baffdd6f046a1391e
Patrick Farrell <pfarrell@ubuntu.(none)>
2012-01-16 14:59:44 -0500

    added report to sitetest

--- test/site-test.rb
+++ test/site-test.rb
@@ -2,10 +2,14 @@ require './helpers'
 require 'uri'
 require 'net/http'
 
+report = {}
 Link.where(:errored=>nil).each do |link|
-  puts link.remote_url
   url = 'http://localhost:4567/' + link.local_url
-  puts url
   r = Net::HTTP.get_response(URI.parse(url))
-  puts r.code
+  report[r.code] = report[r.code].nil? ? 1 : report[r.code] + 1
+end
+
+puts "status\tcount"
+report.each do |key,val|
+  puts key.to_s + "\t" + val.to_s
 end
