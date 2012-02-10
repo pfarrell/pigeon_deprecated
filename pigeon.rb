42e6d6a1a422ba7973f4944e77558e72b4ca575f
Patrick Farrell <pfarrell@Patrick-Farrells-MacBook-Pro.local>
2012-02-09 21:41:01 -0500

    switched processor script to run continuously with a pid file

--- pigeon.rb
+++ pigeon.rb
@@ -16,45 +16,72 @@ def do_gmail(redis, user, stream)
 end
 
 def do_rss(redis, stream)
+  puts stream.url
   rss = get_rss(stream.url)
   rss.items.each do |item|
     enqueue_link(redis, stream, nil, item.link, item.title, Time.new)
   end
 end
 
-redis = Redis.new
-
-User.all().each do |user|
-  Streams.where(:uid=>user.uid).each do |stream|
-    if stream.type.downcase == 'gmail'
-      do_gmail(redis, user, stream)
-    elsif stream.type.downcase == 'rss'
-      do_rss(redis, stream) 
+def validate_pid(file_name)
+  retval = false
+  if !File.exists?(file_name)
+    generate_pid(file_name, $$)
+    retval = true
+  else
+    file_pid = open(file_name).gets
+    begin
+      Process.kill 0, file_pid.to_i
+    rescue
+      generate_pid(file_name, $$)
+      retval = true
     end
   end
+  retval
 end
 
-while(redis.llen('incoming:links') > 0)
-  hash = JSON.parse(redis.lpop('incoming:links'))
+def generate_pid(file_name, process_id)
+  File.open(file_name, 'w+') {|f| f.write($$) }
+end
 
-  link = Link.where(:remote_url=>hash['remote_url']).first
-  if link.nil? 
-    puts 'creating: ' + hash['remote_url']
-    link = Link.new(:title=>hash['title'], :date=>hash['date'], :downloaded=>false, :processed=>false, :remote_url=>hash['remote_url'])
-    link = get_page_contents!(link)
-    link.save!
-  else
-    puts 'link exists: ' + hash['remote_url']
-  end
-  
-  if !link.nil? && !hash['uid'].nil? 
-    if link.downloaded =='false'
-      puts 'downloading: ' + link.remote_url
-      link = get_page!(link)
+if validate_pid("/tmp/pigeon.pid")
+  iter = -1
+  redis = Redis.new
+
+  while(true)
+    iter = iter + 1
+    User.all().each do |user|
+      Streams.where(:uid=>user.uid).each do |stream|
+        if stream.type.downcase == 'gmail'
+          do_gmail(redis, user, stream)
+        elsif stream.type.downcase == 'rss'
+          if iter % 40 == 0  # only parse rss feeds every 15 minutes
+            do_rss(redis, stream) 
+          end
+        end
+      end
     end
-    userlink = Userlink.new(:uid=>hash['uid'], :link_id=>link.id, :date=>hash['date'])
-    userlink.save!
-  else
-    puts 'link status: ' + link.downloaded.to_s
+    
+    while(redis.llen('incoming:links') > 0)
+      hash = JSON.parse(redis.lpop('incoming:links'))
+    
+      link = Link.where(:remote_url=>hash['remote_url']).first
+      if link.nil? 
+        link = Link.new(:title=>hash['title'], :date=>hash['date'], :downloaded=>false, :processed=>false, :remote_url=>hash['remote_url'])
+        link = get_page_contents!(link)
+        link.save!
+      else
+      end
+      
+      if !link.nil? && !hash['uid'].nil? 
+        if link.downloaded =='false'
+          link = get_page!(link)
+        end
+        userlink = Userlink.new(:uid=>hash['uid'], :link_id=>link.id, :date=>hash['date'])
+        userlink.save!
+      else
+      end
+    end
+    sleep(15)
   end
 end
