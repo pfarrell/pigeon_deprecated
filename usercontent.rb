78034b52708e608c61872ba30e34ed99d5459865
Pat Farrell <pfarrell@ulive.com>
2014-06-01 23:36:54 -0700

    loofahed

--- usercontent.rb
+++ usercontent.rb
@@ -1,13 +1,23 @@
 require './helpers'
 
-Userlink.sort(:created_at.desc).each do |ul|
-  link = Link.find(ul.link_id)
-  user = User.find_by_uid(ul.uid)
-  exists = Usercontent.where(:uid=>user.uid, :link_id=>link.id).count
-  if !link.nil? && !user.nil? && exists == 0
-    usercontent = Usercontent.new(:uid=>user.uid, :link_id=>link.id, :title=>link.title, :content=>link.content, :date=>link.created_at)
-    usercontent.save!
-    puts 'saved new usercontent: ' + usercontent.id
+Link.where(:content => nil, :processed=>false).each do |link|
+  begin
+    if(link.remote_url =~ /pdf$/)
+    else
+      puts link.remote_url
+      source = get_html_doc(link.remote_url)
+      doc = Loofah.document(source)
+      doc.scrub!(:whitewash)
+      link.raw_content = source
+      link.processed = true
+      link.content = HTMLEntities.new.decode(doc.text).squeeze(" \t").strip
+      link.save! 
+    end
+  rescue => e
+    link.processed=true
+    link.errored=e.message
+    link.raw_content=''
+    link.content=''
+    link.save! 
   end
 end
-
