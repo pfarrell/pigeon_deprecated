8deb51636ad0eef002c6c6e8d9dd0fec68d75ce4
Pat Farrell <pfarrell@ulive.com>
2014-06-01 23:35:48 -0700

    remove obsolte files

--- dates.rb
+++
@@ -1,5 +0,0 @@
-require './helpers'
-
-Userlink.all.each do |ul|
-puts ul.date.to_s + ': ' + ul.created_at.to_s
-end
--- test.rb
+++
@@ -1,5 +0,0 @@
-require './helpers'
-
-Streams.all.each do |stream|
-  puts stream.id.to_s + ' ' + Link.where(:stream_id=>stream.id.to_s).count.to_s
-end
