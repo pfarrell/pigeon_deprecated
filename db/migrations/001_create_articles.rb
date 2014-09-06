e7f183d8c2a6d9c245891d29548d0da364ddba37
Pat Farrell <pfarrell@ulive.com>
2014-09-06 13:47:00 -0700

    consistent naming

--- db/migrations/001_create_articles.rb
+++ db/migrations/001_create_articles.rb
@@ -3,6 +3,7 @@ Sequel.migration do
     create_table(:articles) do
       primary_key :id
       String :title
+      String :url
       Fixnum :source_id
       DateTime :created_at
       DateTime :updated_at
