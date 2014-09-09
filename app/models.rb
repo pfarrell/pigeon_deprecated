6682e1a59b3a68e123674b6c732f4863dbe5d7bd
Pat Farrell <pfarrell@ulive.com>
2014-09-08 22:23:45 -0700

    env dependent db debug logging

--- app/models.rb
+++ app/models.rb
@@ -4,7 +4,7 @@ require 'logger'
 DB = Sequel.connect(
   "mysql2://#{ENV["PIGEON_DB_USER"]}:#{ENV["PIGEON_DB_PASS"]}@#{ENV["PIGEON_DB_HOST"]}/#{ENV["PIGEON_DB_NAME"]}",
   logger: $console)
-DB.loggers << Logger.new($stdout)
+DB.loggers << Logger.new($stdout) if ENV["PIGEON_DB_DEBUG"] == "true"
 DB.extension(:pagination)
 
 Sequel::Model.plugin :timestamps
