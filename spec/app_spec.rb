65092e7f45c870aa941398f992b7514a87af14ba
Pat Farrell <mr.patfarrell@gmail.com>
2014-10-10 22:14:09 -0700

    more tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,9 +7,31 @@ describe 'Pigeon' do
     expect(last_response.body).to match(/Pigeon/)
   end
 
-  it "has a sources route" do
+  it "has a /sources route" do
     get "/sources"
     expect(last_response).to be_ok
     expect(last_response.body).to match(/Pigeon/)
+  end
+
+  it "has a /source/new route" do
+    get "/source/new"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Pigeon/)
+  end
+
+  it "has a route for a single source" do
+    get "/source/1"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Pigeon/)
+  end
+
+  it "redirects for searches with / as first character" do
+    get "/search?q=/test"
+    expect(last_response).to be_redirect
+  end
+
+  it "searches" do
+    get "/search"
+    expect(last_response).to be_ok
   end
 end
