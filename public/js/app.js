c79c19eae4d85852bd48d16b579267688e50af08
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-02 22:53:55 -0800

    working on overriding dynamic routes to update mthe model

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
-App = Ember.Application.create({});
+App = Ember.Application.create({
+ LOG_TRANSITIONS: true,
+ LOG_VIEW_LOOKUPS: true,
+ LOG_ACTIVE_GENERATION: true
+});
 
 App.ApplicationRoute= Ember.Route.extend({
   setupController: function(controller) {
@@ -12,17 +16,28 @@ App.ApplicationController = Ember.Controller.extend({
 
 App.Router.map(function() {
   this.resource('sources');
-  this.resource('articles');
-});
-
-App.ArticlesRoute = Ember.Route.extend({
-  model: function() {
-    return Ember.$.getJSON('/source/3/articles');
-  }
+  this.resource('source', {path: '/source/:source_id'});
+  this.resource('articles', { path: '/articles/:source_id'});
 });
 
 App.SourcesRoute = Ember.Route.extend({
   model: function() {
     return Ember.$.getJSON('/sources');
+  }
+});
+
+App.SourceRoute = Ember.Route.extend({
+  model: function(params) {
+    return Ember.$.getJSON('/source/' + params.source_id);
+  }
+});
+
+App.ArticlesRoute = Ember.Route.extend({
+  model: function(params) {
+    return Ember.$.getJSON('/source/' + params.source_id + '/articles');
+  },
+  setupController: function(controller, model) {
+    var articles_model = Ember.$.getJSON('/source/' + model.id + '/articles');
+    controller.set("content", articles_model);
   }
 });
