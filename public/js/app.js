c69f4b509ecbe6413740f39abbdb995efd8f05e7
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-04 17:49:38 -0800

    removed cruft

--- public/js/app.js
+++ public/js/app.js
@@ -4,16 +4,6 @@ App = Ember.Application.create({
  LOG_ACTIVE_GENERATION: true
 });
 
-App.ApplicationRoute= Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('title', "Hello world!");
-  }
-});
-
-App.ApplicationController = Ember.Controller.extend({
-    title: 'My First Example'
-});
-
 App.Router.map(function() {
   this.resource('sources', function() {
     this.resource('articles', { path: '/:source_id/articles'});
