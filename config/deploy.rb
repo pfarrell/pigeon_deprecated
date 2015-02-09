3f624e15f2b1ce91b03f29fefaa2b002ac92a24c
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-08 20:36:05 -0800

    ask for branch on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:pfarrell/pigeon.git'
 set :user, 'pfarrell'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/pigeon'
