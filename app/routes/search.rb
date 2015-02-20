b31e33214b034bd1d33bca502ce84cb921023a71
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-20 09:29:13 -0800

    Revert "add order and paging to search queries"
    This reverts commit 6a77c4b669c196634313e6d13b501aa9c51d255f.

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,5 +1,5 @@
 class Pigeon < Sinatra::Application
   get '/search/?' do
-    Article.where(Sequel.ilike(:title, "%#{params[:q]}%")).order(Sequel.desc(:id)).limit(50).to_json
+    Article.where(Sequel.ilike(:title, "%#{params[:q]}%")).to_json
   end
 end
