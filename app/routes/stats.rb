39619325f1efd3e0e64f2b5b5c1ba2bf45468800
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-25 08:21:46 -0800

    switch to straight sql for aggregate query

--- app/routes/stats.rb
+++ app/routes/stats.rb
@@ -1,5 +1,5 @@
 class Pigeon < Sinatra::Application
   get "/stats" do
-    DB[:articles].group_and_count(:source_id).order(Sequel.desc(:count)).all.to_json
+    DB.fetch("SELECT s.title, count(*) from sources s join articles a on a.source_id = s.id group by s.id order by count(*) desc").all.to_json
   end
 end
