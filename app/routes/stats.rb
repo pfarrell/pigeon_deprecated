a8a11c869e8dcc9842c7af25cd0829ceb5f72fbe
Patrick Farrell <mr.patfarrell@gmail.com>
2015-02-27 08:01:27 -0800

    stats template includes links to articles

--- app/routes/stats.rb
+++ app/routes/stats.rb
@@ -1,5 +1,5 @@
 class Pigeon < Sinatra::Application
   get "/stats" do
-    DB.fetch("SELECT s.title, count(*) from sources s join articles a on a.source_id = s.id group by s.id order by count(*) desc").all.to_json
+    DB.fetch("SELECT s.id as source_id, s.title, count(*) from sources s join articles a on a.source_id = s.id group by s.id order by count(*) desc").all.to_json
   end
 end
