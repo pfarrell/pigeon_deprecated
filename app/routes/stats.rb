335a3fa6321ff53080ec9e7dc98d70a56a9113fe
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-11 09:09:27 -0700

    add total article count to stats data

--- app/routes/stats.rb
+++ app/routes/stats.rb
@@ -1,5 +1,7 @@
 class Pigeon < Sinatra::Application
   get "/stats" do
-    DB.fetch("SELECT s.id as source_id, s.title, count(*) from sources s join articles a on a.source_id = s.id group by s.id order by count(*) desc").all.to_json
+    stats=DB.fetch("SELECT s.id as source_id, s.title, count(*) from sources s join articles a on a.source_id = s.id group by s.id order by count(*) desc").all
+    stats << {source_id: -1, title: "Total Articles", count: Article.count}
+    stats.to_json
   end
 end
