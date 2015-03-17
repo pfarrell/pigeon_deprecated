b9fb2787cc239131446119d9aa9cb2a4bc1e3d3b
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-16 17:20:40 -0700

    update domain extraction tool to add rescue for nil urls

--- bin/extract_domains.rb
+++ bin/extract_domains.rb
@@ -1,13 +1,23 @@
 require './app'
 require 'addressable/uri'
 
+ctr=0
 Article.each do |article|
+  ctr += 1
+  if ctr % 500 == 0
+    print '.'
+    STDOUT.flush
+  end
+
   next if article.url.nil? || article.url.empty?
-  next unless article.meta["domain"].nil? || article.meta["domain"].empty?
-  puts article.url
-  uri=Addressable::URI.parse(article.url)
-  meta=article.meta
-  meta[:domain]=uri.host
+  next unless article.meta.nil? || article.meta["domain"].nil? || article.meta["domain"].empty?
+  begin
+    uri=Addressable::URI.parse(article.url)
+    meta=article.meta || {}
+    meta[:domain]=uri.host
+  rescue Exception => ex
+    puts "article: #{article.inspect}"
+  end
   article.meta=meta
   article.save
 end
