1acecb7879e86ca3e5a8517bc0fc19ea7d985f28
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-24 18:02:46 -0700

    removed unused routes after emberjs redux

--- app/routes/sources.rb
+++ app/routes/sources.rb
@@ -5,10 +5,6 @@ class Pigeon < Sinatra::Application
     RssFeed.sort_by{|x| x.title.strip}.to_json
   end
 
-  get "/sources/new" do
-    haml :source
-  end
-
   get "/sources/:id" do
     RssFeed[params[:id].to_i].to_json 
   end
@@ -16,20 +12,6 @@ class Pigeon < Sinatra::Application
   get "/sources/:id/articles" do
     feed = RssFeed[params[:id]]
     Article.where(source: feed).order(Sequel.desc(:id)).limit(100).to_json
-  end
-
-  post "/sources/new" do
-    feed = RssFeed.new(params)
-    feed.save
-    url_for("/sources")
-  end
-
-  post "/sources/:id" do
-    feed = RssFeed[params[:id].to_i]
-    feed.title= params[:title]
-    feed.url = params[:url]
-    feed.save_changes
-    haml :rss_feed, locals: {model: feed}
   end
 
 end
