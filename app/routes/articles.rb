baaa3c00905c2657bec34c8adbce867d331d394d
Patrick Farrell <mr.patfarrell@gmail.com>
2015-05-22 12:23:21 -0700

    size.to_i

--- app/routes/articles.rb
+++ app/routes/articles.rb
@@ -1,6 +1,6 @@
 class Pigeon < Sinatra::Application
   get "/articles/recent" do
-    count = params[:size] || 100
-    Article.last(count).to_json
+    count = params[:size].to_i || 100
+    Article.last(count).to_json(include: :source)
   end
 end
