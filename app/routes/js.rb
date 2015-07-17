c4523d88a7b0df1bb416d5fa892ce85b7f9e1ca3
Patrick Farrell <mr.patfarrell@gmail.com>
2015-07-17 08:43:46 -0700

    moved marklet.rb route file to js.rb

--- app/routes/js.rb
+++ app/routes/js.rb
@@ -4,6 +4,11 @@ class Pigeon < Sinatra::Application
     haml :marklet, :layout=>false, locals: {host: settings.host}
   end
 
+  get "/app.js" do 
+    content_type "application/javascript"
+    haml :app, :layout=>false, locals: {host: settings.host}
+  end
+
   get "/bookmark/new" do
     content_type "application/javascript"
     enqueue("incoming:links", {:url => params[:url], :title=> params[:title], :date=> DateTime.now}.to_json)
