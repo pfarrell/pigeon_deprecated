3b8be74cfd4209c1a91a1c846464d17cc08de752
Patrick Farrell <mr.patfarrell@gmail.com>
2015-07-17 09:55:35 -0700

    search updated to ember-data
    * supports server redirects

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,5 +1,6 @@
 class Pigeon < Sinatra::Application
-  get '/search/?' do
-    Article.where(Sequel.ilike(:title, "%#{params[:q]}%")).order(Sequel.desc(:id)).limit(100).to_json
+  get '/searches/?' do
+    Article.to_json(array: Article.where(Sequel.ilike(:title, "%#{params[:q]}%")).order(Sequel.desc(:id)).limit(100),
+                    root: "searches")
   end
 end
