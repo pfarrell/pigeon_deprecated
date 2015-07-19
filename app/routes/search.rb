82061288f44df1034289120a3784ab03ea0a6db0
Patrick Farrell <mr.patfarrell@gmail.com>
2015-07-19 16:33:54 -0700

    uri decode incomine searches

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,6 +1,7 @@
 class Pigeon < Sinatra::Application
   get '/searches/?' do
-    Article.to_json(array: Article.where(Sequel.ilike(:title, "%#{params[:q]}%")).order(Sequel.desc(:id)).limit(100),
+    search=URI.decode(params[:q])
+    Article.to_json(array: Article.where(Sequel.ilike(:title, "%#{search}%")).order(Sequel.desc(:id)).limit(100),
                     root: "searches")
   end
 end
